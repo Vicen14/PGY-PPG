ad2a74a0f5fd53ae2b6e316e3f2075c693c94772
Vicente Elgueta <vice.elgueta@duocuc.cl>
2025-09-22 15:47:16 -0300

    bd

--- Django/PPG/PPG/settings.py
+++ Django/PPG/PPG/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'PPG.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME': 'XEPDB1',  # O el nombre de tu servicio
+        'USER': 'ppg_admin_s2',
+        'PASSWORD': 'ppg_admin123',
+        'HOST': 'localhost',  # O la IP del servidor
+        'PORT': '1521',
     }
 }
 
